576768471360c1c5ddcd59bb4acaf77a20ae9343
Bobby Rong <rjl931189261@126.com>
2022-02-21 12:55:27 +0800

    what-changed: document the code

--- what-changed.py
+++ what-changed.py
@@ -4,17 +4,20 @@ from git import Repo
 import os
 import subprocess
 
+# Pantheon updates always target the `master` branch
 nixpkgs_flakes = "github:NixOS/nixpkgs"
 work_dir = os.path.join(os.path.dirname(__file__), '.work')
 input_file = os.path.join(os.path.dirname(__file__), 'packages.list')
 output_file = os.path.join(os.path.dirname(__file__), 'README.md')
 
+# Note that we only check whether one of these strings is prefix
+# of the given commit message.
 ignored_keyphrases = [
     'Translated using Weblate',
     'Added translation using Weblate',
     'Deleted translation using Weblate',
     'Update translation files',
-    'Update translation template'
+    'Update translation template'  # Authored by @elementaryBot.
 ]
 
 
@@ -26,6 +29,8 @@ def startswith_ignored_keyphrases(commit_message: str) -> bool:
 
 
 def get_eval(attr: str) -> str:
+    # Probably need nix-command and flakes as experimental features, but they
+    # should be enabled by default already thanks to cachix/install-nix-action.
     return subprocess.run(['nix', 'eval', '--raw', rf"{nixpkgs_flakes}#{attr}"],
                           stdout=subprocess.PIPE, text=True).stdout
 
@@ -34,7 +39,7 @@ def get_dirpath(repo_url: str) -> str:
     return os.path.join(work_dir, repo_url.split('/')[-1])
 
 
-def clone_repo(repo_url: str) -> str:
+def clone_repo(repo_url: str):
     Repo.clone_from(url=repo_url, to_path=get_dirpath(repo_url))
 
 
@@ -44,9 +49,17 @@ def print_title(content: str):
 
 
 def print_log(pkg_attr: str, repo_url: str, from_rev: str):
-    # assuming length of version number wont exceed 16
+    # `from_rev` can be either tag names or git commit hexsha. I assume
+    # length of tag names won't exceed 16. Full git commit hexsha sounds
+    # too long for me.
     from_rev_for_display = from_rev[:16]
     oup = open(output_file, 'a', encoding='utf-8')
+    # Not trying to replace `HEAD` with the actual git commit hexsha as
+    # I only want the output file to be updated when some non-tranlation
+    # commits are made or some tags are created.
+    #
+    # Print 3 extra blank lines here so I can write something on
+    # these lines fast when the output is used in actual review.
     oup.write(
         "\n" + rf"### [{pkg_attr}]({repo_url}): [{from_rev_for_display} → HEAD]({repo_url}/compare/{from_rev}...HEAD)" + "\n\n\n\n")
 
@@ -58,6 +71,10 @@ def print_log(pkg_attr: str, repo_url: str, from_rev: str):
     for commit in repo.iter_commits(rf"{from_rev}..HEAD", reverse=True):
         commit_message_oneline = commit.message.splitlines()[0]
         if commit in tagmap or not startswith_ignored_keyphrases(commit_message_oneline):
+            # Prefixed with `- [ ]` to make this a task list.
+            #
+            # Commit messages are put in <code></code> blocks simply
+            # because this is my personal preference.
             oup.write(
                 rf"- [ ] [<code>{commit_message_oneline}</code>]({repo_url}/commit/{commit.hexsha})")
             if commit in tagmap:
@@ -81,6 +98,8 @@ def main():
             print_title(pkg_attr[1:])
         else:
             repo_url = get_eval(rf"{pkg_attr}.src.meta.homepage")
+            # We don't use `{pkg_attr}.version` to make sure this
+            # works with unstable-YYYY-MM-DD version.
             from_rev = get_eval(rf"{pkg_attr}.src.rev")
             clone_repo(repo_url)
             print_log(pkg_attr, repo_url, from_rev)
