a4da117400ccfdb488903584f38e77f7c935ae3c
Bobby Rong <rjl931189261@126.com>
2022-08-14 15:58:48 +0800

    utils: readd some type hint

--- src/utils.py
+++ src/utils.py
@@ -1,19 +1,20 @@
 import subprocess
+import git
 
 
-def get_eval(flakes_url, attr_path):
+def get_eval(flakes_url: str, attr_path: str):
     return subprocess.run(['nix', 'eval', '--raw', rf"{flakes_url}#{attr_path}"],
                           stdout=subprocess.PIPE, text=True).stdout
 
 
-def get_tagmap(repo):
+def get_tagmap(repo: git.Repo) -> dict:
     tagmap = {}
     for tag in repo.tags:
         tagmap.setdefault(repo.commit(tag), []).append(tag)
     return tagmap
 
 
-def print_commit_tags(tagmap, commit, file):
+def print_commit_tags(tagmap: dict, commit: git.Commit, file: str):
     oup = open(file, 'a', encoding='utf-8')
     if commit in tagmap:
         oup.write("  - <sub>Tagged:")
