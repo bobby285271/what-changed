08935a7181abacba6dc4f3335f6f15bff290f889
Bobby Rong <rjl931189261@126.com>
2022-08-14 23:29:39 +0800

    gitlab: init

---
+++ src/gitlab.py
@@ -0,0 +1,16 @@
+import git
+
+
+def print_title(name: str, url: str, from_rev: str, to_rev: str, file: str):
+    oup = open(file, 'a', encoding='utf-8')
+    oup.write(
+        f"\n#### [{name}]({url}): [{from_rev} → {to_rev}]({url}/-/compare/{from_rev}...{to_rev})\n\n")
+    oup.close()
+
+
+def print_commit(url: str, commit: git.Commit, file: str):
+    oup = open(file, 'a', encoding='utf-8')
+    msg = commit.message.splitlines()[0]
+    oup.write(
+        f"- [ ] [<code>{msg}</code>]({url}/-/commit/{commit.hexsha})\n")
+    oup.close()
