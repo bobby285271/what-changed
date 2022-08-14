c773870a275b2a808ce5102dcbb63ef9d6fb4086
Bobby Rong <rjl931189261@126.com>
2022-08-14 18:30:59 +0800

    what-changed: format

--- what-changed.py
+++ what-changed.py
@@ -5,51 +5,49 @@ import sys
 import src.utils as utils
 import src.printer as printer
 
-nixpkgs_flakes = "local" if utils.debug else "github:NixOS/nixpkgs"
+flake = "local" if utils.debug else "github:NixOS/nixpkgs"
 
 base_dir = os.path.dirname(__file__)
 data_dir = os.path.join(base_dir, 'data')
 work_dir = os.path.join(base_dir, 'work')
 
-input_file = os.path.join(data_dir, rf'{sys.argv[1]}.list')
-output_file = os.path.join(base_dir, rf'{sys.argv[1]}.md')
+in_file = os.path.join(data_dir, rf'{sys.argv[1]}.list')
+out_file = os.path.join(base_dir, rf'{sys.argv[1]}.md')
 
 ignored_msg = utils.get_ignored_msg(os.path.join(data_dir, 'constants.json'))
 
 
 def main():
-    if os.path.exists(output_file):
-        os.remove(output_file)
+    if os.path.exists(out_file):
+        os.remove(out_file)
     if not os.path.exists(work_dir):
         os.makedirs(work_dir)
-    inp = open(input_file, 'r', encoding='utf-8')
-    for pkg_attr in inp.readlines():
-        pkg_attr = rf"{pkg_attr}".strip()
-        # Ignore line starts with '#' and blank line
-        if not pkg_attr or pkg_attr[0] == '#':
+
+    inp = open(in_file, 'r', encoding='utf-8')
+    for line in inp.readlines():
+        line = f"{line}".strip()
+
+        if not line or line[0] == '#':
             continue
-        elif pkg_attr[0] == '@':
-            printer.print_trivial(pkg_attr, output_file)
-        # Track repositories that has no corresponding
-        # packages in Nixpkgs, so far only GitHub repos
-        # are supported
-        elif 'github:' in pkg_attr:
-            repo_url = pkg_attr.split(' ')[0].replace(
-                "github:", "https://github.com/")
-            from_rev = pkg_attr.split(' ')[1]
-            pkg_attr = pkg_attr.split(' ')[0]
-            utils.clone_repo(repo_url, utils.get_dirpath(work_dir, repo_url))
-            printer.print_logs("src.github", work_dir, pkg_attr, repo_url,
-                               from_rev, "HEAD", ignored_msg, output_file)
-        # Track packages updates
+
+        elif line[0] == '@':
+            printer.print_trivial(line, out_file)
+
+        elif line.startswith('github:'):
+            url = line.split(' ')[0].replace("github:", "https://github.com/")
+            from_rev = line.split(' ')[1]
+            line = line.split(' ')[0]
+            utils.clone_repo(url, utils.get_dirpath(work_dir, url))
+            printer.print_logs("src.github", work_dir, line, url,
+                               from_rev, "HEAD", ignored_msg, out_file)
+
         else:
-            repo_url = utils.get_eval(
-                nixpkgs_flakes, f"{pkg_attr}.src.meta.homepage")
-            from_rev = utils.get_eval(nixpkgs_flakes, f"{pkg_attr}.src.rev")
-            utils.clone_repo(repo_url, utils.get_dirpath(work_dir, repo_url))
-            if "github.com" in repo_url:
-                printer.print_logs("src.github", work_dir, pkg_attr, repo_url,
-                                   from_rev, "HEAD", ignored_msg, output_file)
+            url = utils.get_eval(flake, f"{line}.src.meta.homepage")
+            from_rev = utils.get_eval(flake, f"{line}.src.rev")
+            utils.clone_repo(url, utils.get_dirpath(work_dir, url))
+            if "github.com" in url:
+                printer.print_logs("src.github", work_dir, line, url,
+                                   from_rev, "HEAD", ignored_msg, out_file)
 
 
 if __name__ == "__main__":
