a53ffdf095f2b4f0915237c4d14e47d8ce751d23
Bobby Rong <rjl931189261@126.com>
2022-08-14 21:00:50 +0800

    main: simplify

--- src/main.py
+++ src/main.py
@@ -20,6 +20,19 @@ out_file = os.path.join(base_dir, f'{sys.argv[1]}.md')
 ignored_msg = utils.get_ignored_msg(os.path.join(data_dir, 'constants.json'))
 
 
+def fill_data(i):
+    if not "kind" in i:
+        i['kind'] = "github"
+    if not "to_rev" in i:
+        i['to_rev'] = "HEAD"
+    if "attr_path" in i and not "url" in i:
+        i['url'] = utils.get_eval(flake, f"{i['attr_path']}.src.meta.homepage")
+    if "attr_path" in i and not "from_rev" in i:
+        i['from_rev'] = utils.get_eval(flake, f"{i['attr_path']}.src.rev")
+    if "url" in i and not "attr_path" in i:
+        i['attr_path'] = i['url'].split('/')[-1]
+
+
 def main():
     if os.path.exists(out_file):
         os.remove(out_file)
@@ -30,17 +43,11 @@ def main():
     data = json.load(inp, object_pairs_hook=collections.OrderedDict)
 
     for i in data['case']:
-        if i['kind'] == "markdown":
+        if "kind" in i and i['kind'] == "markdown":
             printer.print_trivial(i['content'], out_file)
             continue
 
-        if "attr_path" in i:
-            i['url'] = utils.get_eval(
-                flake, f"{i['attr_path']}.src.meta.homepage")
-            i['from_rev'] = utils.get_eval(flake, f"{i['attr_path']}.src.rev")
-        else:
-            i['attr_path'] = i['url'].split('/')[-1]
-
+        fill_data(i)
         utils.clone_repo(i['url'], utils.get_dirpath(work_dir, i['url']))
         printer.print_logs(i['kind'], work_dir, i['attr_path'], i['url'],
                            i['from_rev'], i['to_rev'], ignored_msg, out_file)
