079b60c039fa07da57aab5f1bf561fbcf47874c7
Bobby Rong <rjl931189261@126.com>
2022-08-14 19:49:18 +0800

    printer: don't ignore the first char

--- src/printer.py
+++ src/printer.py
@@ -5,7 +5,7 @@ import src.utils as utils
 
 def print_trivial(txt: str, file: str):
     oup = open(file, 'a', encoding='utf-8')
-    oup.write(txt[1:] + "\n")  # Ignore the first '@'.
+    oup.write(txt + "\n")
     oup.close()
 
 
