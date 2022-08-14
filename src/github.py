efefc119b590960ea83052eb5315b1a197888c35
Bobby Rong <rjl931189261@126.com>
2022-08-14 15:59:52 +0800

    github: readd some type hint

--- src/github.py
+++ src/github.py
@@ -1,13 +1,14 @@
-# GitHub specific stuff.
+import git
 
-def print_title(name, url, from_rev, to_rev, file):
+
+def print_title(name: str, url: str, from_rev: str, to_rev: str, file: str):
     oup = open(file, 'a', encoding='utf-8')
     oup.write(
         f"\n### [{name}]({url}): [{from_rev} → {to_rev}]({url}/compare/{from_rev}...{to_rev})\n\n")
     oup.close()
 
 
-def print_commit(url, commit, file):
+def print_commit(url: str, commit: git.Commit, file: str):
     oup = open(file, 'a', encoding='utf-8')
     msg = commit.message.splitlines()[0]
     oup.write(
