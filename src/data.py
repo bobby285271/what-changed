cbef13b0e1387f0c2f2f258b250e1cb1d1274acd
Bobby Rong <rjl931189261@126.com>
2022-08-15 09:57:05 +0800

    main: move data fill and check out

---
+++ src/data.py
@@ -0,0 +1,29 @@
+import utils
+
+
+def fill_data(i, flake):
+    if not "kind" in i:
+        i['kind'] = "github"
+    if not "to_rev" in i:
+        i['to_rev'] = "HEAD"
+
+    if "attr_path" in i and not "url" in i:
+        i['url'] = utils.get_eval(flake, f"{i['attr_path']}.src.meta.homepage")
+    if "attr_path" in i and not "from_rev" in i:
+        i['from_rev'] = utils.get_eval(flake, f"{i['attr_path']}.src.rev")
+
+    if "url" in i:
+        i['url'] = i['url'].rstrip('/')
+        if not "attr_path" in i:
+            i['attr_path'] = i['url'].split('/')[-1]
+
+
+def fail_fast_check(i, const_file):
+    if not i['kind'] in utils.get_const(const_file, "supported_kind"):
+        exit(1)
+    if i['kind'] != "markdown":
+        if not "attr_path" in i or not "url" in i or not "from_rev" in i:
+            exit(1)
+    else:
+        if not "content" in i:
+            exit(1)
--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ import collections
 
 import utils
 import printer
+import data
 
 flake = "local" if utils.debug else "github:NixOS/nixpkgs"
 
@@ -19,45 +20,20 @@ in_file = os.path.join(data_dir, f'{sys.argv[1]}.json')
 out_file = os.path.join(base_dir, f'{sys.argv[1]}.md')
 
 
-def fill_data(i):
-    if not "kind" in i:
-        i['kind'] = "github"
-    if not "to_rev" in i:
-        i['to_rev'] = "HEAD"
-
-    if "attr_path" in i and not "url" in i:
-        i['url'] = utils.get_eval(flake, f"{i['attr_path']}.src.meta.homepage")
-    if "attr_path" in i and not "from_rev" in i:
-        i['from_rev'] = utils.get_eval(flake, f"{i['attr_path']}.src.rev")
-
-    if "url" in i:
-        i['url'] = i['url'].rstrip('/')
-        if not "attr_path" in i:
-            i['attr_path'] = i['url'].split('/')[-1]
-
-    if not i['kind'] in utils.get_const(const_file, "supported_kind"):
-        exit(1)
-    if i['kind'] != "markdown":
-        if not "attr_path" in i or not "url" in i or not "from_rev" in i:
-            exit(1)
-    else:
-        if not "content" in i:
-            exit(1)
-
-
 def main():
     inp = open(in_file, 'r', encoding='utf-8')
-    data = json.load(inp, object_pairs_hook=collections.OrderedDict)
+    rdata = json.load(inp, object_pairs_hook=collections.OrderedDict)
 
-    for i in data['case']:
-        fill_data(i)
+    for i in rdata['case']:
+        data.fill_data(i, flake)
+        data.fail_fast_check(i, const_file)
 
     if os.path.exists(out_file):
         os.remove(out_file)
     if not os.path.exists(work_dir):
         os.makedirs(work_dir)
 
-    for i in data['case']:
+    for i in rdata['case']:
         if "kind" in i and i['kind'] == "markdown":
             printer.print_trivial(i['content'], out_file)
             continue
