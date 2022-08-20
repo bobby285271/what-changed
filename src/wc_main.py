43c933bfebd773d0d9b5e033b01ef8b9824ce4dc
Bobby Rong <rjl931189261@126.com>
2022-08-20 15:55:01 +0800

    wc_main: move everything to main()

--- src/wc_main.py
+++ src/wc_main.py
@@ -9,18 +9,18 @@ import wc_utils
 import wc_printer
 import wc_data
 
-flake = "local" if wc_utils.debug else "github:NixOS/nixpkgs"
-
-base_dir = os.path.join(os.path.dirname(__file__), os.pardir)
-data_dir = os.path.join(base_dir, 'data')
-work_dir = os.path.join(base_dir, 'work')
-
-const_file = os.path.join(data_dir, 'constants.json')
-in_file = os.path.join(data_dir, f'{sys.argv[1]}.json')
-out_file = os.path.join(base_dir, f'{sys.argv[1]}.md')
-
 
 def main():
+    flake = "local" if wc_utils.debug else "github:NixOS/nixpkgs"
+
+    base_dir = os.path.join(os.path.dirname(__file__), os.pardir)
+    data_dir = os.path.join(base_dir, 'data')
+    work_dir = os.path.join(base_dir, 'work')
+
+    const_file = os.path.join(data_dir, 'constants.json')
+    in_file = os.path.join(data_dir, f'{sys.argv[1]}.json')
+    out_file = os.path.join(base_dir, f'{sys.argv[1]}.md')
+
     inp = open(in_file, 'r', encoding='utf-8')
     rdata = json.load(inp, object_pairs_hook=collections.OrderedDict)
 
@@ -40,7 +40,7 @@ def main():
 
         wc_utils.clone_repo(i['url'], wc_utils.get_dirpath(work_dir, i['url']))
         wc_printer.print_logs(i['kind'], work_dir, i['attr_path'], i['url'],
-                           i['from_rev'], i['to_rev'], const_file, out_file)
+                              i['from_rev'], i['to_rev'], const_file, out_file)
 
 
 if __name__ == "__main__":
